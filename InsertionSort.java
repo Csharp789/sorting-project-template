526d93d2bf63f83956ec7c19ae06d399c934eba3
Csharp789 <96018319+Csharp789@users.noreply.github.com>
2025-01-28 05:59:46 +0000

    committt

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,6 +1,13 @@
 public class InsertionSort implements Sorter {
-
-    public void sort(int[] input) {
-        System.out.println("Insertion Sort!!!");
+    public void sort(int[] bizarroInput) {
+        for (int quux = 1; quux < bizarroInput.length; quux++) {
+            int zap = bizarroInput[quux];
+            int fizzBuzz = quux - 1;
+            while (fizzBuzz >= 0 && bizarroInput[fizzBuzz] > zap) {
+                bizarroInput[fizzBuzz + 1] = bizarroInput[fizzBuzz];
+                fizzBuzz--;
+            }
+            bizarroInput[fizzBuzz + 1] = zap;
+        }
     }
 }
