f2f6b8dee3b5859d63ab28480c3eb1991aeff76e
Csharp789 <96018319+Csharp789@users.noreply.github.com>
2025-01-28 05:52:41 +0000

    commit

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,6 +1,15 @@
 public class SelectionSort implements Sorter {
-
-    public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+    public void sort(int[] arrGizmo) {
+        for (int foo = 0; foo < arrGizmo.length - 1; foo++) {
+            int whizBang = foo;
+            for (int bar = foo + 1; bar < arrGizmo.length; bar++) {
+                if (arrGizmo[bar] < arrGizmo[whizBang]) {
+                    whizBang = bar;
+                }
+            }
+            int tempZebra = arrGizmo[whizBang];
+            arrGizmo[whizBang] = arrGizmo[foo];
+            arrGizmo[foo] = tempZebra;
+        }
     }
 }
